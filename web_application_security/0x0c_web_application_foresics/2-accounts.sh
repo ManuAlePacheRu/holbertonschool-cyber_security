a83e21e3611115f705206af523ba024813880b68
ManuAlePAcheRu <manualepacheru@gmail.com>
2025-04-08 08:53:25 -0400

    task 2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | awk '/Failed password/ {fail[$NF]++} /Accepted password/ {success[$NF]=1} END {for (u in fail) if (success[u]) print u}'
+tail -n 1000 auth.log | awk '/Failed password/ {fail[$9]++} /Accepted password/ {success[$9]=1} END {for (u in fail) if (success[u]) print u}'
